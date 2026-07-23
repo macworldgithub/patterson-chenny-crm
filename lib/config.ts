75108d4e9e2f09be1884673786435a91f1952582
ayla-imran851 <ayla@macworldtechnology.com>
2026-07-23 12:51:08 +0500

    config updated

--- lib/config.ts
+++ lib/config.ts
@@ -1,3 +1,4 @@
 export const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL ||
-  "https://patterson-backend.vercel.app/api";
+  // process.env.NEXT_PUBLIC_API_BASE_URL ||
+  // "https://patterson-backend.vercel.app/api";
+  "http://localhost:4030/api";
